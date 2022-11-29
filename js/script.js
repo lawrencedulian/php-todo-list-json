8c136c9bfa4ec853f5b634ca6f08c215a150240d
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-29 16:38:02 +0100

    added axios get

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            todoList: []
         };
+    },
+    created() {
+        axios.get("server.php").then((resp) => {
+            this.todoList = resp.data;
+        });
     }
 }).mount("#app");
